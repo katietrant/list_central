c9ed4ee326cc80e928000d2c8d36eaaf2a54f3cb
ktrant84 <katee.trant@gmail.com>
2017-01-08 03:23:00 -0800

    Add react addon

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module Expenses
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+   config.active_record.raise_in_transactional_callbacks = true
+
+   config.react.addons = true
   end
 end

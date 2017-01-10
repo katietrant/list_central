049e3146c87c91597d215d42de1ddebe24f7e957
ktrant84 <katee.trant@gmail.com>
2017-01-09 17:42:21 -0800

    Add add item functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,15 @@ class ItemsController < ApplicationController
 
 
   def new
+    @list= List.find(params[:list_id])
     @item = Item.new
   end
 
   def create
-    @item = Item.create(item_params)
+    @list= List.find(params[:list_id])
+    @item = @list.items.create(item_params)
+    p @item.errors.full_messages
+    redirect_to list_path(@list)
   end
 
   def destroy
@@ -22,7 +26,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:product, :quantity, :list_id)
+    params.require(:item).permit(:product, :quantity)
   end
 
 end

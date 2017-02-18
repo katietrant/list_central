4c3a186f84ecbc41f6296fc47bda1a89f2512983
ktrant84 <katee.trant@gmail.com>
2017-02-18 11:07:35 -0800

    Limit lists routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
   resources :records
-  resources :lists do
+  resources :lists, except: :index do
     resources :items
   end
 

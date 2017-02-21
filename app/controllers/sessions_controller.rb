a3aa6094e33a4444574f2fbf778e481401428791
ktrant84 <katee.trant@gmail.com>
2017-02-20 16:13:11 -0800

    Add tests for user login. Add flash notice for incorrect email/password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-  # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user
-      # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to user_path(session[:user_id])
     else
-    # If user's login doesn't work, send them back to the login form.
+      flash[:notice] = "Your email/password did not match"
       redirect_to login_path
     end
   end

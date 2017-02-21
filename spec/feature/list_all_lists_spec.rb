2bbe47e5eafa7017c91a71c4eb015be49f033970
ktrant84 <katee.trant@gmail.com>
2017-02-20 16:50:49 -0800

    Add test for lists

---
+++ spec/feature/list_all_lists_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require 'spec_helper'
+
+feature "listing user's lists" do
+  scenario "the user visits their homepage and sees all their lists" do
+    user = User.create!(username: "user", email: "user@user.com", password: "password")
+    visit login_path
+    fill_in 'email', with: 'user@user.com'
+    fill_in 'password', with: 'password'
+    click_button 'Login'
+    List.create!(name: "Target", user_id: user.id)
+    visit "/users/show"
+    expect(page).to have_content "Target"
+  end
+end

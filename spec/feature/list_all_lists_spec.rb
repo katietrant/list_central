35ac638185a7e48f6cb79bb6512fd761e7df13bf
ktrant84 <katee.trant@gmail.com>
2017-02-20 16:58:41 -0800

    Add test for items

--- spec/feature/list_all_lists_spec.rb
+++ spec/feature/list_all_lists_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 feature "listing user's lists" do
   scenario "the user visits their homepage and sees all their lists" do
     user = User.create!(username: "user", email: "user@user.com", password: "password")
+    List.create!(name: "Target", user_id: user.id)
     visit login_path
     fill_in 'email', with: 'user@user.com'
     fill_in 'password', with: 'password'
     click_button 'Login'
-    List.create!(name: "Target", user_id: user.id)
     visit "/users/show"
     expect(page).to have_content "Target"
   end
